54a3bc8c2a340bd3b86fcca5b797eb7fcb70fa2d
Youssefguba <youssefguba6@gmail.com>
2018-11-14 17:18:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/youssef/mamafelshare3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.youssef.mamafelshare3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+/* This page Created by : Youssef Guba
+
+    This page is for list of food
+ */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
